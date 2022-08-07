092587e4487665b49a7003b4c5eac7ab3f898ed7
Femi Allo <alloolorunfemi@gmial.com>
2022-08-07 01:10:03 +0100

    added the basic files

---
+++ app.js
@@ -0,0 +1,34 @@
+// const panels = document.querySelectorAll('.panel');
+
+// const toggleOpen = () => {
+//     console.log('hello')
+//     this.classList.toggle('open');
+// }
+
+// const toggleActive = (e) => {
+//     console.log(e.propertyName)
+//     if(e.propertyName.includes('flex')){
+//         this.classList.toggle('open-active');
+//     }
+// }
+// panels.forEach(panel => {
+//     panel.addEventListener('click', toggleOpen)
+//     panel.addEventListener('transitionend', toggleActive)
+// })
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  console.log('Hello');
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
